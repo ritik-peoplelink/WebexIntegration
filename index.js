7fe6afd38085871266f62cfb10310ffd04ffc93c
ritik-peoplelink <ritik@peoplelinkvc.com>
2023-08-30 18:07:29 +0530

    Create Meeting Function Added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import bodyParser from 'body-parser';
-import {authorize,redirect} from './webexHelper.js';
+import {authorize,redirect,createMeeting} from './webexHelper.js';
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,7 +18,10 @@ app.get('/api/authorize/webex',async(req,res)=>{
 
 app.get('/api/webex/redirect', async(req,res)=>{
     return res.json(redirect(req.query.code));
-});
+})
+
+createMeeting();
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
--- webexHelper.js
+++ webexHelper.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const authorize = () => {
     return `https://webexapis.com/v1/authorize?client_id=${process.env.WEBEX_CLIENT_ID}&response_type=code&redirect_uri=${process.env.WEBEX_REDIRECT_URI}&scope=${process.env.WEBEX_SCOPE_LIST}`;
 }
-
+let data={};
 const redirect = async(code) => {
     let tokenURL = 'https://webexapis.com/v1/access_token';
     let payload = {
@@ -16,14 +16,38 @@ const redirect = async(code) => {
     const headers = {
         'Content-Type' : 'application/x-www-form-urlencoded',
     }
-    const data = await axios.post(tokenURL,payload,{headers}).then(response => {
+    data = await axios.post(tokenURL,payload,{headers}).then(response => {
         return response;
     }).catch(e => {
         return e;
     })
-    console.log("Payload ::::::::",payload);
     console.log("Final Data :::::::::::",data);
     return data;
 }
 
-export {authorize,redirect};
+const createMeeting = async()=>
+{
+    const create_meeting_url = "https://webexapis.com/v1/meetings"
+    const headers = {
+        "Authorization": 'Bearer MzY0Y2Y5ZGUtYzRiZC00ZWFlLTg2NTYtNzQ2MjA2OTZlZjFiOWQ5NWZjMmUtZmQ5_P0A1_d753f5ed-fc6f-453b-b80d-9eed175c690b'
+    }
+    const meetingdata = {
+        "title": "My Webex Meeting",
+        "start": "2023-09-01T12:00:00",
+        "end": "2023-09-01T13:00:00",
+        "agenda": "Discuss project updates",
+        "enabledAutoRecordMeeting": false,
+        "allowAnyUserToBeCoHost": false,
+        "password": "Admins@1234",
+        "timezone": "America/New_York"
+    }
+    const responseData = await axios.post(create_meeting_url,meetingdata,{headers}).then(response => {
+        return response;
+    }).catch(e => {
+        return e;
+    })
+    console.log("Final Meeting Data :::::::::::",responseData);
+    return responseData;
+}
+
+export {authorize,redirect,createMeeting};
